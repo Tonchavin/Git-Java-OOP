57342522970ff5c90d77d4d040094e183362325b
Tonchavin Alexandr <bloodwayn@gmail.com>
2023-06-14 17:08:42 +1000

    add HomeWork6

---
+++ src/main/java/work6/src/work1/Rectangle.java
@@ -0,0 +1,16 @@
+package work6.src.work1;
+
+// Прямоугольник
+public class Rectangle {
+
+    int height;
+    int width;
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+}
---
+++ src/main/java/work6/src/work2/ReportGenerator.java
@@ -0,0 +1,6 @@
+package work6.src.work2;
+
+public interface ReportGenerator {
+    String generateXml();
+    String generateJson();
+}
